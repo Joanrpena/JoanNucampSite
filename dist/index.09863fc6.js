35775e37635145aaf0c025411ff817a2583f9c9e
Joan Pena <joan.r.pena1@gmail.com>
2023-09-20 22:41:31 -0400

    first commit

---
+++ dist/index.09863fc6.js
@@ -0,0 +1,17 @@
+console.log("Javascript Connected!");
+const carousel = new bootstrap.Carousel("#homeCarousel", {
+    interval: 2000,
+    pause: false
+});
+const carouselPause = document.getElementById("carouselPause");
+carouselPause.addEventListener("click", function() {
+    console.log("Pausing the carousel");
+    carousel.pause();
+});
+const carouselPlay = document.getElementById("carouselPlay");
+carouselPlay.addEventListener("click", function() {
+    console.log("Cycling the carousel");
+    carousel.cycle();
+});
+
+//# sourceMappingURL=index.09863fc6.js.map
